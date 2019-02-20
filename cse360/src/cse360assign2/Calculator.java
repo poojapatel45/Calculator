997001954eb75f3a2b5514664d9c6c2dafa9a038
Pooja Patel <phpatel3@asu.edu>
2019-02-20 15:18:54 -0700

    Calculator with no changes

---
+++ cse360/src/cse360assign2/Calculator.java
@@ -0,0 +1,77 @@
+/** 
+ * Pooja Patel
+ * Class ID: M 3:05pm (11369)
+ * Assignment 2
+ * Description: this file acts like a calculator, performing addition, multiplication, and division
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the total
+	 * @param none
+	 * @return total
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method adds a value to the total
+	 * @param value - A value to be added to the total
+	 * @return void
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method subtracts a value from the total
+	 * @param value - A value to be subtracted from the total
+	 * @return void
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method multiplies a value to the total
+	 * @param value - A value to be multiplied to the total
+	 * @return void
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method divides the total by the value
+	 * @param value - the total is divided by this value
+	 * @return void
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method returns the history
+	 * @param none
+	 * @return returns the string that contains the history
+	 */
+	
+	public String getHistory () {
+		return "";
+	}
+}
